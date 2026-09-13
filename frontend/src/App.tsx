a982823589b18fb7ed6afaacafa1feadc7c3afca
xAlayham <alayham.aw.1@gmail.com>
2026-09-13 19:18:14 +0100

    Render habit list from array using map and HabitCard component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from 'react'
-
 interface HabitCardProps {
   name: string;
   streak: number;
 }
 
-function HabitCard({name, streak}: HabitCardProps){
-  return <p>{name}: {streak} day streak</p>
+function HabitCard({name, streak}: HabitCardProps) {
+  return <p>{name} : {streak} day streak</p>
 }
 
-function App() {
+function HabitList() {
+  const habits: HabitCardProps[] = [
+    {name: "Nap", streak: 6},
+    {name: "Read", streak: 23},
+    {name: "Exercise", streak: 43}
+  ]
+
   return (
     <div>
-      <HabitCard name="nap" streak={6} />
-      <HabitCard name="read" streak={20} />
+      {habits.map((habit) => ( 
+        <HabitCard key={habit.name} name={habit.name} streak={habit.streak}/>
+      ))}
     </div>
   )
 }
-export default App
+
+export default HabitList
