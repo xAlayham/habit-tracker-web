554e69f99fe4f4c25a22a35652221a3ac0c3dba0
xAlayham <alayham.aw.1@gmail.com>
2026-09-15 14:58:54 +0100

    Add controlled login form with username and password fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect} from "react";
+
 interface HabitCardProps {
   name: string;
   streak: number;
@@ -23,4 +25,69 @@ function HabitList() {
   )
 }
 
-export default HabitList
+interface Habit {
+  name: string
+  version: string
+}
+
+function HabitViewer(){
+  const [habit, setHabit] = useState<Habit | null>(null);
+
+  useEffect(() => {
+    async function loadHabit() {
+      const response = await fetch("http://127.0.0.1:8000/")
+      const data = await response.json();
+      setHabit(data)
+    }
+    loadHabit();
+  }, []);
+
+
+    if (habit === null) {
+      return <p>Loading...</p>
+    }
+    return(
+      <div>
+        <p>{habit.name}</p>
+        <p>{habit.version}</p>
+      </div>
+    );
+}
+
+function LoginForm() {
+  const[username, setUsername] = useState("")
+  const[password, setPassword] = useState("")
+
+  function handleSubmit(e: React.SubmitEvent) {
+    e.preventDefault();
+    console.log("Submitting username:", username, "and password:", password);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <HabitList />
+      <HabitViewer />
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
